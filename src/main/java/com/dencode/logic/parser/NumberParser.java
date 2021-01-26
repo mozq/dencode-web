f91d2b270fe8165195d0d5bc264e6f446452e4ed
mozq <git@mozq.net>
2021-01-26 00:28:40 -0500

    Add number with dot digit separator support

--- src/main/java/com/dencode/logic/parser/NumberParser.java
+++ src/main/java/com/dencode/logic/parser/NumberParser.java
@@ -18,12 +18,15 @@ package com.dencode.logic.parser;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.regex.Pattern;
 
 import org.mifmi.commons4j.util.NumberUtilz;
 import org.mifmi.commons4j.util.StringUtilz;
 import org.mifmi.commons4j.util.exception.NumberParseException;
 
 public class NumberParser {
+	
+	private static final Pattern NUM_DEC_PATTERN_DOT_COMMA = Pattern.compile("[+-]?(?:(?:[0-9]{1,3}(?:\\.[0-9]{3}){2,})|(?:[0-9]{1,3}(?:\\.[0-9]{3})+\\,[0-9]*)|(?:[0-9]{4,}\\,.+)|(?:.+\\,[0-9]{1,2})|(?:.+\\,[0-9]{4,}))");
 	
 	private NumberParser() {
 		// NOP
@@ -35,52 +38,98 @@ public class NumberParser {
 		}
 		
 		val = StringUtilz.toHalfWidth(val, true, true, true, true, false, false);
-		val = val.replace(",", "");
+		val = val.trim();
 		
 		BigDecimal num;
 		
+		// Decimal Format
+		num = parseDec(val);
+		if (num != null) {
+			return num;
+		}
+		
 		// Hex Format
 		if (3 <= val.length() && val.startsWith("0x")) {
-			try {
-				num = new BigDecimal(new BigInteger(val.substring(2), 16));
-			} catch (NumberFormatException e) {
-				num = null;
-			}
+			num = parseHex(val);
 			if (num != null) {
 				return num;
 			}
 		}
 		
-		// Standard Decimal Format
-		try {
-			num = new BigDecimal(val);
-		} catch (NumberFormatException e) {
-			num = null;
-		}
-		if (num != null) {
-			return num;
-		}
-		
 		// English Format
-		try {
-			num = NumberUtilz.parseEnNumShortScale(val);
-		} catch (NumberParseException e1) {
-			num = null;
-		}
+		num = parseEnNumShortScale(val);
 		if (num != null) {
 			return num;
 		}
 		
 		// Japanese Format
-		try {
-			num = NumberUtilz.parseJPNum(val);
-		} catch (NumberParseException e1) {
-			num = null;
-		}
+		num = parseJPNum(val);
 		if (num != null) {
 			return num;
 		}
 		
 		return null;
+	}
+	
+	public static BigDecimal parseDec(String val) {
+		
+		if (NUM_DEC_PATTERN_DOT_COMMA.matcher(val).matches()) {
+			// 0.000.000,0 -> 0000000.0
+			val = val.replace(".", "").replace(",", ".");
+		} else {
+			// 0,000,000.0 -> 0000000.0
+			val = val.replace(",", "");
+		}
+		
+		try {
+			return new BigDecimal(val);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static BigDecimal parseBin(String val) {
+		try {
+			return new BigDecimal(new BigInteger(val, 2));
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static BigDecimal parseOct(String val) {
+		try {
+			return new BigDecimal(new BigInteger(val, 8));
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static BigDecimal parseHex(String val) {
+		if (3 <= val.length() && val.startsWith("0x")) {
+			// Remove hex prefix
+			val = val.substring(2);
+		}
+		
+		try {
+			return new BigDecimal(new BigInteger(val, 16));
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static BigDecimal parseEnNumShortScale(String val) {
+		try {
+			return NumberUtilz.parseEnNumShortScale(val);
+		} catch (NumberParseException e1) {
+			return null;
+		}
+	}
+	
+	public static BigDecimal parseJPNum(String val) {
+		try {
+			return NumberUtilz.parseJPNum(val);
+		} catch (NumberParseException e1) {
+			return null;
+		}
 	}
 }
