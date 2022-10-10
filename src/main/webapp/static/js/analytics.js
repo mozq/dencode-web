47a9863e5a801082ca718476fa555f1b30aa79c0
mozq <git@mozq.net>
2022-10-10 01:06:01 -0400

    Remove Google Analytics UA codes

--- src/main/webapp/static/js/analytics.js
+++ src/main/webapp/static/js/analytics.js
@@ -12,25 +12,14 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-LXQ6W8SL7X');
 
-// Google Analytics for UA
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-44831029-1', 'dencode.com');
-ga('send', 'pageview');
-
 (function (w, d) {
 	$(d).ready(function () {
 		$(d).on("click", "#loadFromFile", function () {
 			gtag("event", "load_file");
-			ga("send", "event", "value", "load-file");
 		});
 		
 		$(d).on("click", "#loadFromQrcode", function () {
 			gtag("event", "load_qrcode");
-			ga("send", "event", "value", "load-qrcode");
 		});
 		
 		$(d).on("click", ".popover-toggle.permanent-link", function () {
@@ -42,17 +31,8 @@ ga('send', 'pageview');
 					content_type: "link",
 					item_id: method
 				});
-				ga("send", "event", "permanent-link", "show", method);
 			}
 		});
-		
-		/*
-		$(d).on("click", "#permanentLink .share-link", function () {
-			var $this = $(this);
-			var shareMethod = $this.attr("data-share-method");
-			ga("send", "event", "permanent-link", "share", shareMethod);
-		});
-		*/
 		
 		$(d).on("click", ".copy-to-clipboard", function () {
 			var $this = $(this);
@@ -62,7 +42,6 @@ ga('send', 'pageview');
 				content_type: "value",
 				item_id: id
 			});
-			ga("send", "event", "value", "copy-to-clipboard", id);
 		});
 		
 		$(d).on("dencoded.dencode", function (ev, data, response) {
@@ -88,7 +67,6 @@ ga('send', 'pageview');
 			var $this = $(this);
 			if ($this.hasClass("active")) {
 				gtag("event", "view_value_length");
-				ga("send", "event", "v-len", "show");
 			}
 		});
 		
@@ -96,10 +74,8 @@ ga('send', 'pageview');
 			var $this = $(this);
 			if ($this.hasClass("active")) {
 				gtag("event", "follow_on");
-				ga("send", "event", "follow", "turn-on");
 			} else {
 				gtag("event", "follow_off");
-				ga("send", "event", "follow", "turn-off");
 			}
 		});
 		
@@ -110,7 +86,6 @@ ga('send', 'pageview');
 				item_list_id: "decoded-list",
 				items: [{item_id: method}]
 			});
-			ga("send", "event", "decoded-list", "select-row", method);
 		});
 		
 		$("#encodedList").find("tr").on("selectrow.dencode", function () {
@@ -120,7 +95,6 @@ ga('send', 'pageview');
 				item_list_id: "encoded-list",
 				items: [{item_id: method}]
 			});
-			ga("send", "event", "encoded-list", "select-row", method);
 		});
 	});
 })(window, document);
