e51fe0bea02df5c2b517580deaf59e0d08efe25b
mozq <git@mozq.net>
2024-01-15 07:19:05 -0500

    Fix copyright year

--- src/main/java/com/dencode/web/server/ServerMain.java
+++ src/main/java/com/dencode/web/server/ServerMain.java
@@ -1,6 +1,6 @@
 /*!
  * dencode-web
- * Copyright 2020 Mozq
+ * Copyright 2016 Mozq
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
